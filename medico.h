79dbb3508bea2d448a6430fbe9fea83f739d2c16
kamimartinez <150206270+kamimartinez@users.noreply.github.com>
2024-05-30 19:26:41 -0600

    Add files via upload

--- medico.h
+++ medico.h
@@ -39,5 +39,78 @@ class medico{
 	}
 	
 	//METODOS
+	
+	virtual void RevisarExamSangre() {
+		cout << "Examen de Sangre revisado por Medico" << endl;
+	}
+};
+
+
+class cardiologo:public medico{
+	private: 
+	string universidad;
+	string experiencia;
+	
+	public:
+	//CONSTRUCTORES
+	cardiologo(): medico(), universidad(""), experiencia("") {};
+	cardiologo(long long id_, string name, int age, string u, string e): medico(id_, name, age), universidad(u), experiencia(e) {};
+	
+	// GETTERS
+	string get_uni() {
+		return universidad;
+	}
+	string get_exp(){
+		return experiencia;
+	}
+	
+	//SETTERS
+	void set_uni(string uni){
+		universidad=uni;
+	}
+	void set_exp(string exp){
+		experiencia=exp;
+	}
+	
+	//METODOS
+	void realizarECG() {
+		cout << "Examen ECG realizado" << endl;
+	}
+	
+	void RevisarExamSangre() {
+		cout << "Examen de Sangre revisado por Cardiologo" << endl;
+	}
+};
+
+class pediatra: public medico {
+	public:
+	int PacientesInfantiles;
+	
+	private: 
+	pediatra():  medico(), PacientesInfantiles(0) {};
+	pediatra(long long id_, string name, int age, int pi): medico(id_, name, age), PacientesInfantiles(pi) {};
+	
+	// GETTERS
+	int get_pi() {
+		return PacientesInfantiles;
+	}
+	
+	//SETTERS
+	void set_pi(int pi) {
+		PacientesInfantiles=pi;
+	}
+	
+	// METODOS
+	void realizarExamenInfantil() {
+		cout << "Examen Infantil realizado" << endl;
+	}
+	
+	void realizarECG(){
+        cout << "El pediatra realiza un ECG adaptado para niños." << std::endl;
+    }
+    
+    void RevisarExamSangre() {
+		cout << "Examen de Sangre revisado por Pediatra" << endl;
+	}
 };
 #endif
