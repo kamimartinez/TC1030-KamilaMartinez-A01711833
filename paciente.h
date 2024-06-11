1d22bd1437b06d008e04c7001467875c4ffb08d2
kamimartinez <150206270+kamimartinez@users.noreply.github.com>
2024-06-11 08:35:47 -0600

    Update paciente.h

--- paciente.h
+++ paciente.h
@@ -3,66 +3,70 @@
 
 #include <bits/stdc++.h>
 #include "medico.h"
-#include "cardiologo.h"
-#include "pediatra.h"
 #include "factura.h"
-#include "tratamiento.h"
 #include "cita.h"
 
 using namespace std;
 
 class Paciente {
-	private:
-    long long id;
-    string nombre;
-    int edad;
-    string telefono;
-    vector<Cita> citas;
-    
-	public:
-    // Constructors
-    Paciente() : id(0), nombre(""), edad(0), telefono("") {}
-    Paciente(long long id_, const string& name, int age, const string& tel) 
-        : id(id_), nombre(name), edad(age), telefono(tel) {}
+    private:
+        long long id;
+        string nombre;
+        int edad;
+        string telefono;
+        Cita citas[10000];
+        int i_c;
 
-    // Getters
-    long long getId() const {
-        return id;
-    }
-    string getNombre() const {
-        return nombre;
-    }
-    int getEdad() const {
-        return edad;
-    }
-    string getTelefono() const {
-        return telefono;
-    }
-    vector<Cita>& getCitas() {
-        return citas;
-    }
+    public:
+        // Constructors
+        Paciente() : id(0), nombre(""), edad(0), telefono(""), i_c(0) {}
+        Paciente(long long id_, string name, int age, string tel) 
+            : id(id_), nombre(name), edad(age), telefono(tel), i_c(0) {}
 
-    // Setters
-    void setId(long long id_) {
-        id = id_;
-    }
-    void setNombre(const string& name) {
-        nombre = name;
-    }
-    void setEdad(int age) {
-        edad = age;
-    }
-    void setTelefono(const string& tel) {
-        telefono = tel;
-    }
+        // Getters
+        long long getId() {
+            return id;
+        }
+        string getNombre() {
+            return nombre;
+        }
+        int getEdad(){
+            return edad;
+        }
+        string getTelefono() {
+            return telefono;
+        }
 
-    // Methods to manage citas
-    void addCita(const Cita& cita) {
-        citas.push_back(cita);
-    }
+        // Setters
+        void setId(long long id_) {
+            id = id_;
+        }
+        void setNombre(string name) {
+            nombre = name;
+        }
+        void setEdad(int age) {
+            edad = age;
+        }
+        void setTelefono(string tel) {
+            telefono = tel;
+        }
 
-
+		void agendar_cita(Cita);
+		void muestra_citas();
 };
 
+void Paciente::agendar_cita(Cita c) {
+    citas[i_c] = c;
+    i_c++;
+}
+
+void Paciente::muestra_citas() {
+    for (int i = 0; i < i_c; i++) {
+        cout << citas[i].toString(); 
+    }
+}
+
 #endif // PACIENTE_H
+
+
 
