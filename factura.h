0f831d76f2dbe8d414ed8e9af54ba6152ba33f2d
kamimartinez <150206270+kamimartinez@users.noreply.github.com>
2024-06-14 04:13:54 -0600

    Add files via upload

--- factura.h
+++ factura.h
@@ -1,3 +1,14 @@
+/*
+ *
+ * Proyecto Integrador clase Factura
+ * Kamila Jeannette Martinez Ibarra
+ * A01711833
+ * 14/06/2024
+ * version : 1
+ * Esta clase define objeto de tipo Factura que contiene atributos y metodos
+ * que colaboran a predecir e imprimir el monto a cobrar
+ */
+
 #ifndef FACTURA_H
 #define FACTURA_H
 
@@ -7,41 +18,60 @@
 using namespace std;
 
 class Factura {
-private:
-    string fecha;
-    float TotalPagar;
+	//Declaramos las variables privadas de instancia
+	private:
+		string fecha;
+		float TotalPagar;
+	
+	//Declaro constructor por default y metodos públicos.
+	public:
+	
+		//CONSTRUCTORES
+		
+		// Constructor por default
+		Factura() : fecha("NoDate"), TotalPagar(0) {}
+		
+		// Constructor con parametros
+		Factura(string date, float tp) : fecha(date), TotalPagar(tp) {}
 
-public:
-    Factura() : fecha("NoDate"), TotalPagar(0) {}
-    Factura(string date, float tp) : fecha(date), TotalPagar(tp) {}
+		// GETTERS
+		string get_fecha() { return fecha; }
+		float get_tp() { return TotalPagar; }
 
-    // GETTERS
-    string get_fecha() const {
-        return fecha;
-    }
-    float get_tp() const {
-        return TotalPagar;
-    }
+		// SETTERS
+		void set_fecha(string date) { fecha = date; }
+		void set_tp(float tp) { TotalPagar = tp; }
 
-    // SETTERS
-    void set_fecha(string date) {
-        fecha = date;
-    }
-    void set_tp(float tp) {
-        TotalPagar = tp;
-    }
-
-    // METODOS
-    void hacerFactura() const {
-        cout << "Fecha de Facturación: " << get_fecha() << endl;
-        cout << "Total a pagar: $" << get_tp() << endl;
-    }
-
-    void agregarMonto(float monto) {
-        TotalPagar += monto;
-    }
+		// METODOS
+		void hacerFactura();
+		void agregarMonto(float);
 };
 
-#endif // FACTURA_H
+/**
+ * hacerFactura imprime la fecha y monto a cobrar
+ *
+ * Imprime la fecha y monto a cobrar
+ *
+ * @param 
+ * @return
+ */
+void Factura::hacerFactura() {
+        cout << "Fecha de Facturación: " << get_fecha() << endl;
+        cout << "Total a pagar: $" << get_tp() << endl;
+}
 
+/**
+ * agregarMonto agrega monto a TotalPagar
+ *
+ * Agrega el monto recibido en el parametro y lo agrega a la variable
+ * TotalPagar
+ *
+ * @param float monto
+ * @return
+ */
+void Factura::agregarMonto(float monto) {
+        TotalPagar += monto;
+}
+
+#endif // FACTURA_H
 
