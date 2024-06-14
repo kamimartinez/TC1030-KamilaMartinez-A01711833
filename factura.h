84326e29ee069ecd051d900872bc83c566320013
kamimartinez <150206270+kamimartinez@users.noreply.github.com>
2024-06-13 19:30:44 -0600

    Update factura.h

--- factura.h
+++ factura.h
@@ -2,42 +2,46 @@
 #define FACTURA_H
 
 #include <bits/stdc++.h>
+#include <string>
 
 using namespace std;
 
 class Factura {
-    private:
-        string fecha;
-        float TotalPagar;
+private:
+    string fecha;
+    float TotalPagar;
 
-    public:
-        Factura() : fecha("NoDate"), TotalPagar(0) {};
-        Factura(string date, float tp) : fecha(date), TotalPagar(tp) {};
+public:
+    Factura() : fecha("NoDate"), TotalPagar(0) {}
+    Factura(string date, float tp) : fecha(date), TotalPagar(tp) {}
 
-        // GETTERS
-        string get_fecha() {
-            return fecha;
-        }
-        float get_tp() {
-            return TotalPagar;
-        }
+    // GETTERS
+    string get_fecha() const {
+        return fecha;
+    }
+    float get_tp() const {
+        return TotalPagar;
+    }
 
-        // SETTERS
-        void set_fecha(string date) {
-            fecha = date;
-        }
-        void set_tp(float tp) {
-            TotalPagar = tp;
-        }
+    // SETTERS
+    void set_fecha(string date) {
+        fecha = date;
+    }
+    void set_tp(float tp) {
+        TotalPagar = tp;
+    }
 
-        // METODOS
-        void hacerFactura() {
-            cout << "Fecha de Facturación: " << get_fecha() << endl;
-            cout << "Total a pagar: $" << get_tp() << endl;
-        }
+    // METODOS
+    void hacerFactura() const {
+        cout << "Fecha de Facturación: " << get_fecha() << endl;
+        cout << "Total a pagar: $" << get_tp() << endl;
+    }
 
-        void agregarMonto(float monto) {
-            TotalPagar += monto;
-        }
+    void agregarMonto(float monto) {
+        TotalPagar += monto;
+    }
 };
+
 #endif // FACTURA_H
+
+
